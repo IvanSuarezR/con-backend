be9be186e89a4392374c12c9877249db1dae6f34
IvanSuarezR <ivancitozuarez@gmail.com>
2025-12-17 14:09:17 -0400

    Despliegue final004 error

--- condominio_proyecto/condominio_BackendAPI/settings.py
+++ condominio_proyecto/condominio_BackendAPI/settings.py
@@ -162,7 +162,7 @@ WSGI_APPLICATION = 'condominio_BackendAPI.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "examen1",
+        "NAME": os.environ.get("DB_NAME", "postgres"),
         "USER": "postgres",
         "PASSWORD": os.environ.get("DB_PASSWORD", "2427"),
         "HOST": os.environ.get("DB_HOST", "localhost"),
